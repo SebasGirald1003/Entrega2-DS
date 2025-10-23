2015ab9a0f4a4969864738a792e6e0eb656e9c78
jjvasquezg <juanjos137k@gmail.com>
2025-10-22 23:19:30 -0500

    config.py Actualizado

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
-SECRET_KEY = 'secretkey'
+DB_USER = os.getenv("DB_USER", "bookstore_user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "bookstore_pass")
+DB_HOST = os.getenv("DB_HOST", "localhost")  # En producción: IP PRIVADA del EC2-DB
+DB_NAME = os.getenv("DB_NAME", "bookstore")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecret")
